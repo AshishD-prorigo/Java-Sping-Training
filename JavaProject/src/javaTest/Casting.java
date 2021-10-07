e00ef99c05a577b1902ca4a998b64fb4c1d812b8
ashishd <ashishd@AshishD-PC.prorigo.com>
2021-10-07 19:51:54 +0530

    practice on casting

---
+++ JavaProject/src/javaTest/Casting.java
@@ -0,0 +1,17 @@
+package javaTest;
+//data type casting
+public class Casting {
+
+	public static void main(String[] args) {
+		//approch 1
+		int a=20;
+		short b=30;
+		short c=(short)(b+a);//explicit casting
+		
+		//approch 2
+		int v=c;//implicit casting
+		
+
+	}
+
+}
