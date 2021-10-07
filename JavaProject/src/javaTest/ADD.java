7d7136eeed7e4a65323b40223cfb96a4257d1253
AshishD-prorigo <92074023+AshishD-prorigo@users.noreply.github.com>
2021-10-07 16:38:55 +0530

    update in calculation

--- JavaProject/src/javaTest/ADD.java
+++ JavaProject/src/javaTest/ADD.java
@@ -5,6 +5,10 @@ public class ADD {
 	public static void main(String[] args) {
 		System.out.println("Pune");
 		System.out.println("Nashik");
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println(c);
 		
 
 	}
