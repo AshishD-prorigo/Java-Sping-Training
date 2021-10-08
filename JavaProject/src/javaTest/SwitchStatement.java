f0d66891c1afcd346bec71bc80edb8f4fc54e069
ashishd <ashishd@AshishD-PC.prorigo.com>
2021-10-08 16:22:08 +0530

    switch with array element

--- JavaProject/src/javaTest/SwitchStatement.java
+++ JavaProject/src/javaTest/SwitchStatement.java
@@ -3,33 +3,45 @@ package javaTest;
 public class SwitchStatement {
 
 	public static void main(String[] args) {
-		int num1=60;
-		int num2=50;
-		double result=0d;
-		char opCode='d';
-		switch(opCode) 
+		int num1[]= {40,50,60,70};
+		int num2[]= {10,20,30,40};
+		
+		double result[]=new double[num1.length];
+		
+		char opCode[]= {'a','s','m','d'};
+		
+		for(int i=0;i<num2.length;i++) 
+		{
+			
+		switch(opCode[i]) 
 		{
 		case 'a':
-			result=num1+num2;
+			result[i]=num1[i]+num2[i];
 			break;
 		
 		case 's':
-			result=num1-num2;
+			result[i]=num1[i]-num2[i];
 			break;
 		
 		case 'm':
-			result=num1*num2;
+			result[i]=num1[i]*num2[i];
 			break;
 		
 		case 'd':
-			result=num2!=0?num1/num2:0.0d;
+			result[i]=num2[i]!=0?num1[i]/num2[i]:0.0d;
 			break;
 			
 		default:
-			System.out.println("enter valid opcode:"+opCode);
+			System.out.println("enter valid opcode:"+opCode[i]);
 			break;
 		}
-		System.out.println(result);
+		
+		}
+		for(double r:result)
+		{
+			System.out.println(r);
+		}
+		
 
 	}
 
