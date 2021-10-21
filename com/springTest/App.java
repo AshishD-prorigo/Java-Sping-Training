0c2986cecb2d07b0f58fdf934696379365e158be
ashish.prorigo <ashish.damahe@prorigosoftware.com>
2021-10-21 17:15:54 +0530

    This is practice program For Constructor Dependecy Injectio with multiple object

--- com/springTest/App.java
+++ com/springTest/App.java
@@ -12,10 +12,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext context= new ClassPathXmlApplicationContext("ConstructorConfig.xml");
         
         Car car1=(Car)context.getBean("car1");
         System.out.println(car1);
+        
+        Car car2=(Car)context.getBean("car2");
+        System.out.println(car2);
     }
 
 
